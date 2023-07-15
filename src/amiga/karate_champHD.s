9e4d51664f825a9f4ea1208b17cfa6bc26880801
Jean-François Fabre <jotd666@gmail.com>
2023-07-15 16:51:30 +0200

    debugging boot

--- src/amiga/karate_champHD.s
+++ src/amiga/karate_champHD.s
@@ -9,7 +9,7 @@ _base	SLAVE_HEADER					; ws_security + ws_id
 	dc.w	17					; ws_version (was 10)
 	dc.w	WHDLF_NoError
     IFD CHIP_ONLY
-	dc.l	$100000					; ws_basememsize
+	dc.l	$200000					; ws_basememsize
     ELSE
 	dc.l	$100000					; ws_expmem
     ENDC
