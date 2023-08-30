c8d4a07a355329420ae4abebd70cb43a4d448c56
Jean-François Fabre <jotd666@gmail.com>
2023-08-31 00:20:54 +0200

    fixed nasty last-minute bugs

--- create_amiga_archive.py
+++ create_amiga_archive.py
@@ -1,4 +1,4 @@
-import subprocess,zipfile,os
+import subprocess,os,glob,shutil
 
 progdir = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,12 +12,18 @@ subprocess.check_call(cmd_prefix+["clean"],cwd=os.path.join(progdir,"src"))
 
 subprocess.check_call(cmd_prefix+["RELEASE_BUILD=1"],cwd=os.path.join(progdir,"src"))
 # create archive
-with zipfile.ZipFile(os.path.join(progdir,"KarateChamp500_HD.zip"),"w",compression=zipfile.ZIP_DEFLATED) as zf:
-    for file in ["readme.md","instructions.txt",gamename,f"{gamename}.slave"]:
-        zf.write(os.path.join(progdir,file),arcname=file)
 
-    zf.write(os.path.join(progdir,"assets","amiga","KarateChamp.info"),"KarateChamp.info")
-    zf.write(os.path.join(progdir,"assets","amiga","boxart.png"),"boxart.png")
+outdir = os.path.join(progdir,f"{gamename}_HD")
+if os.path.exists(outdir):
+    for x in glob.glob(os.path.join(outdir,"*")):
+        os.remove(x)
+else:
+    os.mkdir(outdir)
+for file in ["readme.md","instructions.txt",gamename,f"{gamename}.slave"]:
+    shutil.copy(os.path.join(progdir,file),outdir)
+
+shutil.copy(os.path.join(progdir,"assets","amiga","KarateChamp.info"),outdir)
+shutil.copy(os.path.join(progdir,"assets","amiga","boxart.png"),outdir)
 
 # pack the file for floppy
 subprocess.check_output(["cranker_windows.exe","-f",os.path.join(progdir,gamename),"-o",os.path.join(progdir,f"{gamename}.rnc")])
