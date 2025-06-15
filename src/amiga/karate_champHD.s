776b223dc12b962de96524b0ceabf9ebfbb886d8
Jean-François Fabre <jotd666@gmail.com>
2025-06-15 23:12:04 +0200

    memory specs just dropped :)

--- src/amiga/karate_champHD.s
+++ src/amiga/karate_champHD.s
@@ -9,9 +9,9 @@ _base	SLAVE_HEADER					; ws_security + ws_id
 	dc.w	17					; ws_version (was 10)
 	dc.w	WHDLF_NoError
     IFD CHIP_ONLY
-	dc.l	$200000					; ws_basememsize
+	dc.l	$100000					; ws_basememsize
     ELSE
-	dc.l	$E0000					; ws_expmem
+	dc.l	$80000					; ws_expmem
     ENDC
 	dc.l	0					; ws_execinstall
 	dc.w	start-_base		; ws_gameloader
@@ -25,7 +25,7 @@ _expmem
     IFD CHIP_ONLY
     dc.l    $0
     ELSE
-	dc.l	$100000					; ws_expmem
+	dc.l	$E0000					; ws_expmem
     ENDC
 	dc.w	_name-_base				; ws_name
 	dc.w	_copy-_base				; ws_copy
@@ -85,7 +85,7 @@ start:
     
     IFD CHIP_ONLY
     lea  _expmem(pc),a0
-    move.l  #$C0000,(a0)
+    move.l  #$2000,(a0)
     ENDC
     lea progstart(pc),a0
     move.l  _expmem(pc),(a0)
